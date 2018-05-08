8a9276f5ba9622a9fb6e717c06cfbd1380fcfc1d
zkffkvltl <zkffkvltl@gmail.com>
2018-05-08 12:57:37 +0900

    first commit

---
+++ about.vue
@@ -0,0 +1,7 @@
+<template>
+ <section class="section">
+ <div class="block">
+      This is a test.
+ </div>
+ </section>
+</template>
---
+++ index.vue
@@ -0,0 +1,11 @@
+<template>
+ <section class="section">
+ <div class="block">
+ <a href="#" class="button is-primary">Test</a>
+ <p>클라우드 서비스용 포트폴리오</p>
+ <img src="" alt=""><br>
+ <img src="" alt=""><br>
+ </div>
+ <nuxt-link to="/about">About</nuxt-link>
+ </section>
+</template>
